c84582edcbf2d8459c9595e2a7115d8353b3117c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-17 12:55:30 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['expired on'] = 'abgelaufen am';
+$lang['expires on'] = 'läuft ab am';
+$lang['less than %s days'] = 'Weniger als %s Tage';
+$lang['more than than %s days'] = 'Mehr als %s Tage';
+$lang['remove expiry date'] = 'Verfallsdatum entfernen';
+$lang['the past (already expired)'] = 'die Vergangenheit (bereits verfallen)';
+$lang['You have expiring photos'] = 'Sie haben ablaufende Fotos';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Sie haben diese E-Mail erhalten, weil Sie zuvor diese Fotos heruntergeladen haben: %s';
+$lang['You must select an album for archiving photos'] = 'Sie müssen ein Album für die Archivierung von Fotos auswählen';
+$lang['To change this setting go to:'] = 'Um diese Einstellung zu ändern, gehen Sie zu:';
+$lang['User notifications'] = 'Benutzerbenachrichtigungen';
+$lang['What should piwigo do once photos expire'] = 'Was sollte Piwigo tun, wenn die Fotos abgelaufen sind?';
+$lang['Where should Piwigo move archived photos'] = 'Wohin sollte Piwigo archivierte Fotos verschieben?';
+$lang['Therefore these images have been archived in the album %s'] = 'Daher wurden diese Bilder im Album %s archiviert.';
+$lang['These images have reached expiration: %s'] = 'Diese Bilder haben das Verfallsdatum erreicht: %s';
+$lang['These photo have reached their expiry date.'] = 'Diese Fotos haben ihr Verfallsdatum erreicht.';
+$lang['These photos will soon expire.'] = 'Dieses Foto wird bald ablaufen';
+$lang['Notify downloaders of photo expiration'] = 'Benachrichtigung der Downloader über den Ablauf von Fotos';
+$lang['Notify users before the expiry date'] = 'Benachrichtigung der Nutzer vor Ablauf der Frist';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Am Ablaufdatum wird eine E-Mail an alle Administratoren gesendet';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Nach Ablauf der Gültigkeitsdauer wird eine E-Mail verschickt, um alle Personen zu benachrichtigen, die das Foto heruntergeladen haben.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo weiß nur, ob ein Nutzer das Foto heruntergeladen hat, wenn sein Besuchsverlauf gespeichert ist.';
+$lang['No action was taken on these images'] = 'Es wurden keine Maßnahmen zu diesen Bildern ergriffen.';
+$lang['No prenotification'] = 'Keine Vorankündigung';
+$lang['Notify admins before the expiry date'] = 'Benachrichtigung der Administratoren vor Ablauf der Frist';
+$lang['Notify admins of photo expiration'] = 'Benachrichtigen Sie Administratoren über den Ablauf von Fotos';
+$lang['Expires in'] = 'Läuft ab in';
+$lang['Expiring photos'] = 'Auslaufende Fotos';
+$lang['Expiry date'] = 'Verfallsdatum';
+$lang['Expiry date, action has been taken'] = 'Verfallsdatum, Maßnahmen wurden ergriffen';
+$lang['Administrator notifications'] = 'Administrator-Benachrichtigungen';
+$lang['Archive expired photos'] = 'Abgelaufene Fotos archivieren';
+$lang['Delete expired photos'] = 'Abgelaufene Fotos löschen';
+$lang['Do nothing'] = 'Nichts tun';
+$lang['Add additional content to email'] = 'Hinzufügen zusätzlicher Inhalte zur E-Mail';
+$lang['%s, in %s days'] = '%s, in %s Tagen ';
+$lang['14 days before expiry'] = '14 Tage vor Ablauf der Gültigkeit';
+$lang['30 days before expiry'] = '30 Tage vor Ablauf der Gültigkeit';
+$lang['7 days before expiry'] = '7 Tage vor Ablauf der Gültigkeit';
+$lang['A set number of days before a email will be sent'] = 'Eine bestimmte Anzahl von Tagen, bevor eine E-Mail gesendet wird, um darauf hinzuweisen, dass die Fotos in Zukunft ablaufen werden';
+$lang['Action on expiration'] = 'Aktion zum Ablauf der Frist';
