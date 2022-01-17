91a3a96ef21b4f873c95b3057339eb4d9f11b10c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-17 13:04:31 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'fortiden (allerede udløbet)';
+$lang['Notify users before the expiry date'] = 'Giv brugere besked før udløbsdato';
+$lang['On the expiry date, an email will be sent to all admins'] = 'På udløbsdatoen sendes en mail til alle administratorer';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'På udløbsdatoen sendes en mail til alle der har hentet fotografieret.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo ved kun om en bruger har hentet fotografiet, hvis deres besøgshistorik gemmes.';
+$lang['Therefore these images have been archived in the album %s'] = 'Derfor er disse billeder blevet arkiveret i albummet %s';
+$lang['These images have reached expiration: %s'] = 'Disse billeder udløber: %s';
+$lang['These photo have reached their expiry date.'] = 'Disse fotografier har nået deres udløbsdato.';
+$lang['These photos will soon expire.'] = 'Disse fotografier vil snart udløbe';
+$lang['To change this setting go to:'] = 'For at ændre denne indstilling, gå til: ';
+$lang['User notifications'] = 'Brugernotifikationer';
+$lang['What should piwigo do once photos expire'] = 'Hvad skal Piwigo gøre når fotografier udløber?';
+$lang['Where should Piwigo move archived photos'] = 'Hvorhen skal Piwigo flytte arkiverede fotografier?';
+$lang['You have expiring photos'] = 'Du har fotografier, som udløber';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Du modtager denne mail, fordi du tidligere har hentet disse fotografier: %s';
+$lang['You must select an album for archiving photos'] = 'Du skal vælge et album til arkivering af fotografier';
+$lang['expired on'] = 'udløbet den';
+$lang['expires on'] = 'udløber den';
+$lang['less than %s days'] = 'Mindre end %s dage';
+$lang['more than than %s days'] = 'Mere end %s dage';
+$lang['remove expiry date'] = 'fjern udløbsdato';
+$lang['%s, in %s days'] = '%s, om %s dage ';
+$lang['14 days before expiry'] = '14 dage før udløb';
+$lang['30 days before expiry'] = '30 dage før udløb';
+$lang['7 days before expiry'] = '7 dage før udløb';
+$lang['A set number of days before a email will be sent'] = 'Et bestemt antal dage før, vil en mail blive sendt med besked om at fotografierne udløber i fremtiden';
+$lang['Action on expiration'] = 'Handling ved udløb';
+$lang['Add additional content to email'] = 'Tilføj yderligere indhold til mailen';
+$lang['Administrator notifications'] = 'Administratornotifikationer';
+$lang['Archive expired photos'] = 'Arkivér udløbne fotografier';
+$lang['Delete expired photos'] = 'Slet udløbne fotografier';
+$lang['Do nothing'] = 'Gør intet';
+$lang['Expires in'] = 'Udløber om';
+$lang['Expiring photos'] = 'Udløbende fotografier';
+$lang['Expiry date'] = 'Udløbsdato';
+$lang['Expiry date, action has been taken'] = 'Udløbsdato, handling er udført';
+$lang['No action was taken on these images'] = 'Ingen handling udført på disse billeder.';
+$lang['No prenotification'] = 'Ingen før-notifikation';
+$lang['Notify admins before the expiry date'] = 'Giv administratorer besked før udløbsdatoen';
+$lang['Notify admins of photo expiration'] = 'Giv administratorer besked om fotografiudløb';
+$lang['Notify downloaders of photo expiration'] = 'Giv downloadere besked om fotografiudløb';
