2c382425ebbe79512aa2248d8694a6fab0c73b5e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-17 13:43:31 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['less than %s days'] = 'Manj kot %s dni';
+$lang['more than than %s days'] = 'Več kot %s dni';
+$lang['remove expiry date'] = 'odstranite datum veljavnosti';
+$lang['the past (already expired)'] = 'preteklost (že potekla)';
+$lang['expired on'] = 'poteče dne';
+$lang['expires on'] = 'poteče dne';
+$lang['You must select an album for archiving photos'] = 'Izbrati morate album za arhiviranje slik';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'To e-pošto ste prejeli, ker ste predhodno prenesli te slike: %s';
+$lang['You have expiring photos'] = 'Imate slike, ki potečejo';
+$lang['What should piwigo do once photos expire'] = 'Kaj naj naredi Piwigo, ko slike potečejo?';
+$lang['Where should Piwigo move archived photos'] = 'Kam naj Piwigo premakne arhivirane slike?';
+$lang['User notifications'] = 'Obvestila uporabnikov';
+$lang['To change this setting go to:'] = 'Če želite spremeniti to nastavitev, pojdite na:';
+$lang['These photos will soon expire.'] = 'Te slike bodo kmalu potekle';
+$lang['These images have reached expiration: %s'] = 'Te slike so potekle: %s';
+$lang['These photo have reached their expiry date.'] = 'Tem slikam je potekel datum veljavnosti.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo ve, ali je uporabnik prenesel sliko, samo če je shranjena njegova zgodovina obiskov.';
+$lang['Therefore these images have been archived in the album %s'] = 'Zato so bile te slike arhivirane v albumu %s';
+$lang['Notify admins before the expiry date'] = 'Obvesti skrbnike pred iztekom roka';
+$lang['Notify admins of photo expiration'] = 'Obvesti skrbnike o poteku slike';
+$lang['Notify downloaders of photo expiration'] = 'Obvestite uporabnike, ki prenašajo slike, o poteku slike';
+$lang['Notify users before the expiry date'] = 'Obvestite uporabnike pred iztekom roka';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Na datum poteka veljavnosti bo e-poštno sporočilo poslano vsem skrbnikom';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Na datum poteka veljavnosti bo poslano e-poštno sporočilo z obvestilom vsem, ki so prenesli sliko.';
+$lang['No prenotification'] = 'Brez predhodnega obvestila';
+$lang['No action was taken on these images'] = 'V zvezi s temi slikami ni bilo sprejetih nobenih ukrepov.';
+$lang['Expiry date'] = 'Datum veljavnosti';
+$lang['Expiry date, action has been taken'] = 'Datum veljavnosti, ukrepanje je bilo sprejeto';
+$lang['Expires in'] = 'Poteče v';
+$lang['Expiring photos'] = 'Slike, ki potečejo';
+$lang['Action on expiration'] = 'Ukrep ob poteku';
+$lang['Add additional content to email'] = 'Dodajte dodatno vsebino v e-pošto';
+$lang['Administrator notifications'] = 'Obvestila skrbnika';
+$lang['Archive expired photos'] = 'Arhivirajte potekle slike';
+$lang['Delete expired photos'] = 'Izbriši potekle slike';
+$lang['Do nothing'] = 'Ne naredi ničesar';
+$lang['%s, in %s days'] = '%s, čez %s dni ';
+$lang['14 days before expiry'] = '14 dni pred iztekom';
+$lang['30 days before expiry'] = '30 dni pred iztekom';
+$lang['7 days before expiry'] = '7 dni pred iztekom';
+$lang['A set number of days before a email will be sent'] = 'Določeno število dni, preden bo poslano e-poštno sporočilo z obvestilom, da slike potečejo v prihodnosti';
