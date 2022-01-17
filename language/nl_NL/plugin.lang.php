38411d00c0cd32f5ecbe6dbb0f8b53710155d6e6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-17 17:32:21 +0100

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Notify users before the expiry date'] = 'Gebruikers op de hoogte stellen vóór de vervaldatum';
+$lang['These images have reached expiration: %s'] = 'Deze afbeeldingen zijn verlopen: %s';
+$lang['These photos will soon expire.'] = 'Deze foto\'s verlopen binnenkort';
+$lang['To change this setting go to:'] = 'Ga om deze instelling te wijzigen naar:';
+$lang['User notifications'] = 'Gebruikersmeldingen';
+$lang['What should piwigo do once photos expire'] = 'Wat moet Piwigo doen als foto\'s verlopen zijn?';
+$lang['Where should Piwigo move archived photos'] = 'Waar moet Piwigo gearchiveerde foto\'s naar toe verplaatsen?';
+$lang['You have expiring photos'] = 'U hebt verlopen foto\'s';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'U hebt deze e-mail ontvangen omdat je eerder deze foto\'s hebt gedownload: %s';
+$lang['You must select an album for archiving photos'] = 'U moet een album selecteren om foto\'s te archiveren';
+$lang['expired on'] = 'Verlopen op';
+$lang['expires on'] = 'Verloopt op';
+$lang['less than %s days'] = 'Minder dan %s dagen';
+$lang['more than than %s days'] = 'Meer dan %s dagen';
+$lang['remove expiry date'] = 'vervaldatum verwijderen';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Op de vervaldatum wordt een e-mail verzonden naar alle beheerders';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo weet alleen of een gebruiker de foto heeft gedownload als zijn bezoekgeschiedenis is opgeslagen.';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Op de vervaldatum wordt een e-mail verzonden om iedereen die de foto heeft gedownload op de hoogte te stellen.';
+$lang['Therefore these images have been archived in the album %s'] = 'Daarom zijn deze afbeeldingen gearchiveerd in het album %s';
+$lang['These photo have reached their expiry date.'] = 'Deze foto\'s hebben hun vervalsdatum bereikt.';
+$lang['the past (already expired)'] = 'het verleden (reeds verlopen)';
+$lang['%s, in %s days'] = '%s, in %s dagen';
+$lang['14 days before expiry'] = '14 dagen voor de vervaldatum';
+$lang['30 days before expiry'] = '30 dagen voor de vervaldatum';
+$lang['7 days before expiry'] = '7 dagen voor de vervaldatum';
+$lang['A set number of days before a email will be sent'] = 'Een bepaald aantal dagen voordat er een e-mail wordt verzonden om te melden dat foto\'s in de toekomst verlopen';
+$lang['Action on expiration'] = 'Actie na afloop';
+$lang['Add additional content to email'] = 'Extra inhoud aan e-mail toevoegen';
+$lang['Administrator notifications'] = 'Beheerdersmeldingen';
+$lang['Archive expired photos'] = 'Vervallen foto\'s archiveren';
+$lang['Delete expired photos'] = 'Vervallen foto\'s verwijderen';
+$lang['Do nothing'] = 'Niets doen';
+$lang['Expires in'] = 'Verloopt in';
+$lang['Expiring photos'] = 'Verlopen foto\'s';
+$lang['Expiry date'] = 'Vervaldatum';
+$lang['Expiry date, action has been taken'] = 'Vervaldatum, actie is ondernomen';
+$lang['No action was taken on these images'] = 'Op deze foto\'s is geen actie ondernomen.';
+$lang['No prenotification'] = 'Geen voormelding';
+$lang['Notify admins before the expiry date'] = 'Breng beheerders op de hoogte vóór de vervaldatum';
+$lang['Notify admins of photo expiration'] = 'Breng beheerders op de hoogte van het verlopen van foto\'s';
+$lang['Notify downloaders of photo expiration'] = 'Breng downloaders op de hoogte van het verlopen van foto\'s';
