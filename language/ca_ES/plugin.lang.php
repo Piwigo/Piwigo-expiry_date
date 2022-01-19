06ce3c96ceb730ed6f6a0717088349cdc1ba006c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-19 18:31:35 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s, in %s days'] = '%s, en %s dies';
+$lang['14 days before expiry'] = '14 dies abans de caducar';
+$lang['30 days before expiry'] = '30 dies abans de caducar';
+$lang['7 days before expiry'] = '7 dies abans de caducar';
+$lang['A set number of days before a email will be sent'] = 'Un nombre determinat de dies abans que s\'enviï un missatge de correu electrònic per notificar que les fotos caducaran en el futur';
+$lang['Action on expiration'] = 'Acció en caducar';
+$lang['Add additional content to email'] = 'Afegeix contingut addicional al missatge';
+$lang['Administrator notifications'] = 'Notificacions de l\'administrador';
+$lang['Archive expired photos'] = 'Arxiva les fotos caducades';
+$lang['Delete expired photos'] = 'Suprimeix les fotos caducades';
+$lang['Do nothing'] = 'No facis res';
+$lang['Expires in'] = 'Caduca en';
+$lang['Expiring photos'] = 'Fotos que caduquen';
+$lang['Expiry date'] = 'Data de caducitat';
+$lang['Expiry date, action has been taken'] = 'Data de caducitat, s\'ha dut a terme una acció';
+$lang['No action was taken on these images'] = 'No s\'ha dut a terme cap acció sobre aquestes imatges';
+$lang['No prenotification'] = 'No hi ha pre-notificació';
+$lang['Notify admins before the expiry date'] = 'Notifica els administradors abans de la data de caducitat';
+$lang['Notify admins of photo expiration'] = 'Notifica els administradors de la caducitat de les fotos';
+$lang['Notify downloaders of photo expiration'] = 'Notifica de la caducitat de les fotos als qui les hagin descarregat';
+$lang['Notify users before the expiry date'] = 'Notifica els usuaris abans de la data de caducitat';
+$lang['On the expiry date, an email will be sent to all admins'] = 'En la data de caducitat s\'enviarà un missatge de correu electrònic a tots els administradors';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'En la data de caducitat s\'enviarà un missatge de correu electrònic a tothom que hagi descarregat la foto';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo només sap si un usuari ha descarregat la foto si es desa el seu historial de visites.';
+$lang['Therefore these images have been archived in the album %s'] = 'Per tant aquestes imatges s\'han arxivat a l\'àlbum %s';
+$lang['These images have reached expiration: %s'] = 'Aquestes imatges han arribat a la seva caducitat: %s';
+$lang['These photo have reached their expiry date.'] = 'Aquestes fotos han arribat a la seva data de caducitat.';
+$lang['These photos will soon expire.'] = 'Aquesta foto caducarà aviat';
+$lang['To change this setting go to:'] = 'Per canviar aquest paràmetre aneu a:';
+$lang['User notifications'] = 'Notificacions d\'usuari';
+$lang['What should piwigo do once photos expire'] = 'Què ha de fer el Piwigo quan caduquin les fotos?';
+$lang['Where should Piwigo move archived photos'] = 'On ha de moure les fotos arxivades el Piwigo?';
+$lang['You have expiring photos'] = 'Teniu fotos que caduquen';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Heu rebut aquest missatge perquè havíeu descarregat prèviament aquestes fotos: %s';
+$lang['You must select an album for archiving photos'] = 'Heu de seleccionar un àlbum on arxivar les fotos';
+$lang['expired on'] = 'caducada el';
+$lang['expires on'] = 'caduca el';
+$lang['less than %s days'] = 'Menys de %s dies';
+$lang['more than than %s days'] = 'Més de %s dies';
+$lang['remove expiry date'] = 'elimina la data de caducitat';
+$lang['the past (already expired)'] = 'el passat (ja ha caducat)';
