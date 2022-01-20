8efb4618542a112d631e94c11b66236d0ae7c87f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-20 10:55:48 +0100

    Update cs_CZ, thanks to webprostor

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'minulost (už expirovaly)';
+$lang['Notify users before the expiry date'] = 'Upozorni uživatele před datem expirace';
+$lang['On the expiry date, an email will be sent to all admins'] = 'V den vypršení platnosti bude všem administrátorům zaslán e-mail';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Po uplynutí doby platnosti bude odeslán e-mail, který upozorní každého, kdo si fotografii stáhl.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo ví, zda si uživatel fotografii stáhl, pouze pokud je uložena historie jeho návštěv.';
+$lang['Therefore these images have been archived in the album %s'] = 'Proto byly tyto obrázky archivovány v albu %s';
+$lang['These images have reached expiration: %s'] = 'Tyto obrázky expirují: %s';
+$lang['These photo have reached their expiry date.'] = 'Tyto fotky epxirovaly.';
+$lang['These photos will soon expire.'] = 'Tyto fotky budou brzy expirovat';
+$lang['To change this setting go to:'] = 'Pro změnu tohoto nastavení přejděte na:';
+$lang['User notifications'] = 'Upozornění uživatele';
+$lang['What should piwigo do once photos expire'] = 'Co má Piwigo udělat po vypršení platnosti fotografií?';
+$lang['Where should Piwigo move archived photos'] = 'Kam má Piwigo archivovat fotky?';
+$lang['You have expiring photos'] = 'Máte expirující fotky';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'obdržel jste tuto zprávu protože jste si ukládal tyto fotky: %s';
+$lang['You must select an album for archiving photos'] = 'Musíte zvolit album pro archivaci fotek';
+$lang['expired on'] = 'expirovalo dne';
+$lang['expires on'] = 'expiruje dne';
+$lang['less than %s days'] = 'Za méně než %s dnů';
+$lang['more than than %s days'] = 'Více než %s dnů';
+$lang['remove expiry date'] = 'odebrat datum expirace';
+$lang['%s, in %s days'] = '%s, v %s dnech';
+$lang['14 days before expiry'] = '14 dnů před expirací';
+$lang['30 days before expiry'] = '30 dnů před expirací';
+$lang['7 days before expiry'] = '7 dnů před expirací';
+$lang['A set number of days before a email will be sent'] = 'Zvolte počet dnů před expirací kdy bude odeslána zpráva s upozorněním na expiraci fotek.';
+$lang['Action on expiration'] = 'Akce při expiraci';
+$lang['Add additional content to email'] = 'Vložit další obsah do zprávy';
+$lang['Administrator notifications'] = 'Notifikace správce';
+$lang['Archive expired photos'] = 'Archivace expirovaných fotek';
+$lang['Delete expired photos'] = 'Odstranit expirované fotky';
+$lang['Do nothing'] = 'Neprovádět nic';
+$lang['Expires in'] = 'Expiruje v';
+$lang['Expiring photos'] = 'Expirující fotky';
+$lang['Expiry date'] = 'Datum expirace';
+$lang['Expiry date, action has been taken'] = 'Datum expirace, akce byla provedena';
+$lang['No action was taken on these images'] = 'Na těchto obrázcích nebyla provedena žádná akce.';
+$lang['No prenotification'] = 'Bez upozornění';
+$lang['Notify admins before the expiry date'] = 'Upozornit správce před datem expirace';
+$lang['Notify admins of photo expiration'] = 'Upozornit správce na expiraci fotek';
+$lang['Notify downloaders of photo expiration'] = 'Upozornit při stažení na expiraci fotografie';
