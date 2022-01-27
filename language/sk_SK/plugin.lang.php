9c9944e342d26e2ad3fe8e513b48a7d354ca18bc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-27 14:58:58 +0100

    Update sk_SK, thanks to jose1711

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Action on expiration'] = 'Akcia pri vypršaní doby';
+$lang['Add additional content to email'] = 'Pridať dodatočný obsah do e-mailu';
+$lang['14 days before expiry'] = '14 dní pred vypršaním doby';
+$lang['30 days before expiry'] = '30 dní pred vypršaním doby';
+$lang['7 days before expiry'] = '30 dní pred vypršaním doby';
