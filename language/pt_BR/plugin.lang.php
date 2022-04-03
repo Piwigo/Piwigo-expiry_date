dffe28dffcb89ab19d078af74909df203849ba26
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-03 18:35:55 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'o passado (já expirado)';
+$lang['Notify users before the expiry date'] = 'Notifique os usuários antes da data de expiração';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Na data de expiração, um e-mail será enviado a todos os administradores';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Na data de expiração, um e-mail será enviado para notificar quem tiver baixado a foto.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo sabe se um usuário baixou a foto somente se seu histórico de visitas estiver salvo.';
+$lang['Therefore these images have been archived in the album %s'] = 'Portanto, estas imagens foram arquivadas no álbum %s';
+$lang['These images have reached expiration: %s'] = 'Estas imagens atingiram a validade: %s';
+$lang['These photo have reached their expiry date.'] = 'Estas fotos atingiram a data de validade.';
+$lang['These photos will soon expire.'] = 'Estas fotos irão expirar em breve';
+$lang['To change this setting go to:'] = 'Para alterar esta configuração acesse:';
+$lang['User notifications'] = 'Notificações do usuário';
+$lang['What should piwigo do once photos expire'] = 'O que Piwigo deve fazer quando as fotos expirarem?';
+$lang['Where should Piwigo move archived photos'] = 'Para onde o Piwigo deve mover as fotos arquivadas?';
+$lang['You have expiring photos'] = 'Você tem fotos expirando';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Você recebeu este e-mail porque baixou estas fotos anteriormente: %s';
+$lang['You must select an album for archiving photos'] = 'Você deve selecionar um álbum para arquivar fotos';
+$lang['expired on'] = 'Expirou em';
+$lang['expires on'] = 'expira em';
+$lang['less than %s days'] = 'Menos de %s dias';
+$lang['more than than %s days'] = 'Mais de %s dias';
+$lang['remove expiry date'] = 'remover data de expiração';
+$lang['%s, in %s days'] = '%s, em %s dias ';
+$lang['14 days before expiry'] = '14 dias antes do vencimento';
+$lang['30 days before expiry'] = '30 dias antes do vencimento';
+$lang['7 days before expiry'] = '7 dias antes do vencimento';
+$lang['A set number of days before a email will be sent'] = 'Um número definido de dias antes de um e-mail ser enviado para notificar que as fotos estão expirando no futuro';
+$lang['Action on expiration'] = 'Ação na expiração';
+$lang['Add additional content to email'] = 'Adicionar conteúdo adicional ao e-mail';
+$lang['Administrator notifications'] = 'Notificações do administrador';
+$lang['Archive expired photos'] = 'Arquivar fotos expiradas';
+$lang['Delete expired photos'] = 'Excluir fotos expiradas';
+$lang['Do nothing'] = 'Não fazer nada';
+$lang['Expires in'] = 'Expira em';
+$lang['Expiring photos'] = 'Fotos expirando';
+$lang['Expiry date'] = 'Data de validade';
+$lang['Expiry date, action has been taken'] = 'Data de expiração, ação foi executada';
+$lang['No action was taken on these images'] = 'Nenhuma ação foi tomada nessas imagens.';
+$lang['No prenotification'] = 'Sem pré-notificação';
+$lang['Notify admins before the expiry date'] = 'Notifique os administradores antes da data de expiração';
+$lang['Notify admins of photo expiration'] = 'Notifique os administradores sobre a expiração da foto';
+$lang['Notify downloaders of photo expiration'] = 'Notifique quem baixa os arquivos sobre a expiração da foto';
