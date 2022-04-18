69171bc44f2e61b2e69a2551950f57d26aef6a88
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-18 23:01:54 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'fortíðin (þegar útrunninn)';
+$lang['Notify users before the expiry date'] = 'Láttu notendur vita fyrir fyrningardagsetningu';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Á fyrningadagsetningunni verður tölvupóstur sendur til allra stjórnenda';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Á fyrningadagsetningunni verður tölvupóstur sendur til að láta alla vita sem hafa halað niður myndinni.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo veit aðeins hvort notandi hefur hlaðið niður myndinni ef heimsóknarferill hans er vistaður.';
+$lang['Therefore these images have been archived in the album %s'] = 'Þess vegna hafa þessar myndir verið geymdar í albúminu %s';
+$lang['These images have reached expiration: %s'] = 'Þessar myndir eru að renna út: %s';
+$lang['These photo have reached their expiry date.'] = 'Þessar myndir hafa náð sinni fyrningardagsetningu.';
+$lang['These photos will soon expire.'] = 'Þessar myndir munu fljótlega renna út';
+$lang['To change this setting go to:'] = 'Til að breyta þessari stillingu skaltu fara á:';
+$lang['User notifications'] = 'Notandatilkynningar';
+$lang['What should piwigo do once photos expire'] = 'Hvað á Piwigo að gera þegar myndir renna út?';
+$lang['Where should Piwigo move archived photos'] = 'Hvert ætti Piwigo að flytja geymdar myndir?';
+$lang['You have expiring photos'] = 'Þú ert með myndir sem eru að renna út';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Þú hefur fengið þennan tölvupóst vegna þess að þú hefur áður hlaðið niður þessum myndum: %s';
+$lang['You must select an album for archiving photos'] = 'Þú verður að velja albúm til að geyma myndir';
+$lang['expired on'] = 'rann út þann';
+$lang['expires on'] = 'rennur út þann';
+$lang['less than %s days'] = 'Minna en %s dagar';
+$lang['more than than %s days'] = 'Meira en %s dagar';
+$lang['remove expiry date'] = 'fjarlægja fyrningardagsetningu';
+$lang['%s, in %s days'] = '%s, eftir %s daga';
+$lang['14 days before expiry'] = '14 dögum fyrir fyrningu';
+$lang['30 days before expiry'] = '30 dögum fyrir fyrningu';
+$lang['7 days before expiry'] = '7 dögum fyrir fyrningu';
+$lang['A set number of days before a email will be sent'] = 'Tiltekinn fjöldi daga áður en tölvupóstur verður sendur til að tilkynna að myndir séu að renna út';
+$lang['Action on expiration'] = 'Aðgerð við fyrningu';
+$lang['Add additional content to email'] = 'Bættu viðbótarefni við tölvupóst';
+$lang['Administrator notifications'] = 'Tilkynningar stjórnanda';
+$lang['Archive expired photos'] = 'Settu útrunnar myndir í geymslu';
+$lang['Delete expired photos'] = 'Eyða útrunnum myndum';
+$lang['Do nothing'] = 'Gera ekkert';
+$lang['Expires in'] = 'Fyrnist eftir';
+$lang['Expiring photos'] = 'Myndir að fyrnast';
+$lang['Expiry date'] = 'Fyrningardagsetning';
+$lang['Expiry date, action has been taken'] = 'Fyrningardagsetning, gripið hefur verið til aðgerða';
+$lang['No action was taken on these images'] = 'Ekkert var gert við þessar myndir.';
+$lang['No prenotification'] = 'Engin fyrirfram tilkynning';
+$lang['Notify admins before the expiry date'] = 'Láttu stjórnendur vita fyrir fyrningardagsetningu';
+$lang['Notify admins of photo expiration'] = 'Látið stjórnendur vita að mynd rennur út';
+$lang['Notify downloaders of photo expiration'] = 'Látið niðurhalsaðila vita að mynd rennur út';
