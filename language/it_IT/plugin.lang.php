2a840ff54e2a5ff6df55f5b440545c7271f8b971
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-18 18:51:58 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'Già scaduta';
+$lang['To change this setting go to:'] = 'Per cambiare questa impostazione vai a:';
+$lang['User notifications'] = 'Notifiche dell\'utente';
+$lang['What should piwigo do once photos expire'] = 'Cosa deve fare Piwigo quando le fotografie raggiungono la data di scadenza?';
+$lang['Where should Piwigo move archived photos'] = 'Dove devono essere archiviate da Piwigo le fotografie?';
+$lang['You have expiring photos'] = 'Hai fotografie in scadenza';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Hai ricevuto questa email perché precedentemente hai scaricato queste fotografie: %s';
+$lang['You must select an album for archiving photos'] = 'Devi selezionare un album per poter archiviare le fotografie';
+$lang['expired on'] = 'Scaduta(e) il';
+$lang['expires on'] = 'Scade il';
+$lang['less than %s days'] = 'Meno di %s giorni';
+$lang['more than than %s days'] = 'Più di %s giorni';
+$lang['remove expiry date'] = 'Rimuovi la data di scadenza';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo può sapere se l\'utente ha scaricato una fotografia solo se la cronologia delle visite è viene salvata.';
+$lang['Therefore these images have been archived in the album %s'] = 'Pertanto le immagini sono state archiviate nell\'album %s';
+$lang['These images have reached expiration: %s'] = 'Queste immagini hanno raggiunto la scadenza: %s';
+$lang['These photo have reached their expiry date.'] = 'Queste fotografie hanno raggiunto la loro data di scadenza';
+$lang['These photos will soon expire.'] = 'Queste fotografie scadranno in breve tempo';
+$lang['Notify users before the expiry date'] = 'Notifica gli utenti prima della data di scadenza';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Alla data di scadenza, un\'email verrà inviata a tutti gli amministratori';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Alla data di scadenza un\'email verrà inviata a tutti coloro i quali hanno scaricato la fotografia';
+$lang['%s, in %s days'] = '%s, in %s giorni';
+$lang['14 days before expiry'] = '14 giorni alla scadenza';
+$lang['30 days before expiry'] = '30 giorni alla scadenza';
+$lang['7 days before expiry'] = '7 giorni alla scadenza';
+$lang['A set number of days before a email will be sent'] = 'Numero impostato di giorni prima che un\'email venga spedita per notificare la scadenza delle fotografie';
+$lang['Action on expiration'] = 'Azione alla scadenza';
+$lang['Add additional content to email'] = 'Aggiungi altro contenuto all\'email';
+$lang['Administrator notifications'] = 'Notifiche dell\'ammministratore';
+$lang['Archive expired photos'] = 'Archivia le fotografie scadute';
+$lang['Delete expired photos'] = 'Cancella le fotofgrafie scadute';
+$lang['Do nothing'] = 'Non fare nulla';
+$lang['Expires in'] = 'Scade in';
+$lang['Expiring photos'] = 'Fotografie in scadenza';
+$lang['Expiry date'] = 'Data di scadenza';
+$lang['Expiry date, action has been taken'] = 'Data di scadenza, è stata intrapresa un\'azione';
+$lang['No action was taken on these images'] = 'Nessun azione è stata intrapresa su queste immagini';
+$lang['No prenotification'] = 'Nessuna notifica anticipata';
+$lang['Notify admins before the expiry date'] = 'Notifica gli amministratori prima della data di scadenza';
+$lang['Notify admins of photo expiration'] = 'Notifica gli amministratori sulla scadenza della fotografia';
+$lang['Notify downloaders of photo expiration'] = 'Notifica sulla scadenza della fotografia chi la scarica';
