bd3ff22fe51a3cf990a949c0839a2c654950cf30
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-03 16:50:52 +0100

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'geçmiş (zaten süresi dolmuş)';
+$lang['Notify users before the expiry date'] = 'Sona erme tarihinden önce kullanıcıları bilgilendirin';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Sona erme tarihinde tüm yöneticilere bir e-posta gönderilecektir.';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Sona erme tarihinde, fotoğrafı indiren herkesi bilgilendirmek için bir e-posta gönderilecektir.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo, bir kullanıcının fotoğrafı indirip indirmediğini ancak ziyaret geçmişi kaydedildiyse bilir.';
+$lang['Therefore these images have been archived in the album %s'] = 'Bu nedenle bu görüntüler %s albümünde arşivlendi';
+$lang['These images have reached expiration: %s'] = 'Bu resimlerin süresi doldu: %s';
+$lang['These photo have reached their expiry date.'] = 'Bu fotoğrafların süresi son kullanma tarihine ulaştı.';
+$lang['These photos will soon expire.'] = 'Bu fotoğrafların süresi yakında dolacak';
+$lang['To change this setting go to:'] = 'Bu ayarı değiştirmek için şuraya gidin:';
+$lang['User notifications'] = 'Kullanıcı bildirimleri';
+$lang['What should piwigo do once photos expire'] = 'Fotoğrafların süresi dolduktan sonra Piwigo ne yapmalı?';
+$lang['Where should Piwigo move archived photos'] = 'Piwigo arşivlenen fotoğrafları nereye taşımalı?';
+$lang['You have expiring photos'] = 'Süresi dolan fotoğraflarınız var';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Bu e-postayı daha önce şu fotoğrafları indirdiğiniz için aldınız: %s';
+$lang['You must select an album for archiving photos'] = 'Fotoğrafları arşivlemek için bir albüm seçmelisiniz';
+$lang['expired on'] = 'süresi doldu';
+$lang['expires on'] = 'tarihinde sona eriyor';
+$lang['less than %s days'] = '%s günden az';
+$lang['more than than %s days'] = '%s günden fazla';
+$lang['remove expiry date'] = 'sona erme tarihini kaldır';
+$lang['%s, in %s days'] = '%s, %s gün içinde';
+$lang['14 days before expiry'] = '14 gün dolmadan önce';
+$lang['30 days before expiry'] = '30 gün dolmadan önce';
+$lang['7 days before expiry'] = '7 gün dolmadan önce';
+$lang['A set number of days before a email will be sent'] = 'Son kullanma tarihinden 7, 14 veya 30 gün önce bir e-posta göndermek için bir ön bildirim oluşturun.';
+$lang['Action on expiration'] = 'Son kullanma tarihiyle ilgili eylem';
+$lang['Add additional content to email'] = 'E-postaya ek içerik ekleyin';
+$lang['Administrator notifications'] = 'Yönetici bildirimleri';
+$lang['Archive expired photos'] = 'Süresi dolmuş fotoğrafları arşivle';
+$lang['Delete expired photos'] = 'Süresi dolmuş fotoğrafları sil';
+$lang['Do nothing'] = 'Hiçbir şey yapma';
+$lang['Expires in'] = 'İçinde sona eriyor';
+$lang['Expiring photos'] = 'Süresi dolan fotoğraflar';
+$lang['Expiry date'] = 'Sona erme tarihi';
+$lang['Expiry date, action has been taken'] = 'Sona erme tarihi, işlem yapıldı';
+$lang['No action was taken on these images'] = 'Bu görüntüler üzerinde herhangi bir işlem yapılmadı.';
+$lang['No prenotification'] = 'Ön bildirim yok';
+$lang['Notify admins before the expiry date'] = 'Sona erme tarihinden önce yöneticileri bilgilendirin';
+$lang['Notify admins of photo expiration'] = 'Yöneticileri fotoğraf süresinin sona ermesi konusunda bilgilendirin';
+$lang['Notify downloaders of photo expiration'] = 'İndirme yapanları fotoğraf süresinin sona ermesi konusunda bilgilendirin';
