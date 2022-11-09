17a8d5fc033bc4e9efb074ce7160524e83c51fb8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-09 20:13:39 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['No prenotification'] = 'Pas de pré-notification';
+$lang['On the expiry date, an email will be sent to all admins'] = 'A la date d\'expiration, un courriel sera envoyé à tous les administrateurs';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'A la date d\'expiration, un courriel sera envoyé à chaque personne ayant téléchargé la photo.';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Vous avez reçu ce courriel car vous avez précédemment téléchargé ces photos : %s';
+$lang['A set number of days before a email will be sent'] = 'Créez une pré-notification pour envoyer un courriel 7, 14 ou 30 jours avant la date d\'expiration';
+$lang['Add additional content to email'] = 'Ajoutez un contenu supplémentaire au courriel de notification';
