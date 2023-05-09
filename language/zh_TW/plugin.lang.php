908bb9d1478de27610c2bf175f9b2e63b4b11608
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-09 11:10:05 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You must select an album for archiving photos'] = '您必須選擇存放封存照片的相簿';
+$lang['expired on'] = '已於此日到期';
+$lang['expires on'] = '將於此日到期';
+$lang['less than %s days'] = '少於 %s 天';
+$lang['more than than %s days'] = '超過 %s 天';
+$lang['remove expiry date'] = '移除到期日設定';
+$lang['the past (already expired)'] = '過去（已經過期）';
+$lang['User notifications'] = '使用者通知';
+$lang['What should piwigo do once photos expire'] = '當照片到期時，Piwigo 該怎麼做？';
+$lang['Where should Piwigo move archived photos'] = 'Piwigo 該把封存的照片移至何處？';
+$lang['You have expiring photos'] = '您有將到期的照片';
+$lang['You have received this email because you previously downloaded these photos: %s'] = '您會收到這封電子郵件，是因為您之前曾經下載過這些照片：%s';
+$lang['To change this setting go to:'] = '將此設定更改：';
+$lang['These photos will soon expire.'] = '這些照片快到期了';
+$lang['These photo have reached their expiry date.'] = '這些照片已到期。';
+$lang['These images have reached expiration: %s'] = '這些影像已到期：%s';
+$lang['Therefore these images have been archived in the album %s'] = '因此這些影像都被封存在 %s 相簿中';
+$lang['Notify users before the expiry date'] = '在到期日之前通知使用者';
+$lang['On the expiry date, an email will be sent to all admins'] = '在到期日當天，透過電子郵件通知給所有的管理者';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = '使用者的瀏覽記錄必須要儲存下來，Piwigo 才能知道使用者是否曾下載此照片。';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = '在到期日當天，透過電子郵件通知所有曾經下載此照片的人員。';
+$lang['Expires in'] = '距到期日還有';
+$lang['Expiring photos'] = '設定照片到期日';
+$lang['Expiry date, action has been taken'] = '到期日，動作已執行';
+$lang['No action was taken on these images'] = '並未對這些圖片執行任何動作。';
+$lang['No prenotification'] = '沒有預告通知';
+$lang['Notify admins before the expiry date'] = '在到期日前通知管理者';
+$lang['Notify admins of photo expiration'] = '在照片到期時通知管理者';
+$lang['Notify downloaders of photo expiration'] = '在照片到期時通知下載者';
+$lang['Expiry date'] = '到期日';
+$lang['Do nothing'] = '什麼都不做';
+$lang['Delete expired photos'] = '刪除到期的照片';
+$lang['Archive expired photos'] = '封存到期的照片';
+$lang['Administrator notifications'] = '管理者通知';
+$lang['Add additional content to email'] = '新增額外的內容至電子郵件';
+$lang['Action on expiration'] = '到期時要執行的動作';
+$lang['%s, in %s days'] = '%s，還有 %s 天';
+$lang['14 days before expiry'] = '還有 14 天到期';
+$lang['30 days before expiry'] = '還有 30 天到期';
+$lang['7 days before expiry'] = '還有 7 天到期';
+$lang['A set number of days before a email will be sent'] = '建立預告通知，在到期日前 7 天、14 天，或 30 天前透過電子郵件通知';
