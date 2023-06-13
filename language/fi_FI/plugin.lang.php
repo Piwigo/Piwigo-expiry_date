98d23df9aeef0a8599665c94ab65455ca124298c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-13 06:06:29 +0200

    Update fi_FI, thanks to nikke

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'menneisyys (jo vanhentunut)';
+$lang['You must select an album for archiving photos'] = 'Sinun on valittava albumi valokuvien arkistointia varten';
+$lang['expired on'] = 'vanhentunut';
+$lang['expires on'] = 'vanhentuu';
+$lang['less than %s days'] = 'Vähemmän kuin %s päivää';
+$lang['more than than %s days'] = 'Enemmän kuin %s päivää';
+$lang['remove expiry date'] = 'poista vanhentumispäivä';
+$lang['Notify users before the expiry date'] = 'Muistuta käyttäjiä ennen vanhentumispäivää';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Vanhenemispäivänä kaikille ylläpitäjille lähetetään sähköposti';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Vanhenemispäivänä lähetetään sähköpostiviesti, joka ilmoittaa kaikille kuvan ladanneille.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo tietää, onko käyttäjä ladannut kuvan vain, jos hänen vierailuhistoriansa on tallennettu.';
+$lang['Therefore these images have been archived in the album %s'] = 'Siksi nämä kuvat on arkistoitu albumiin %s';
+$lang['These images have reached expiration: %s'] = 'Nämä kuvat ovat vanhentuneet: %s';
+$lang['These photo have reached their expiry date.'] = 'Nämä kuvat ovat saavuttaneet vanhenemispäivän.';
+$lang['These photos will soon expire.'] = 'Nämä kuvat vanhentuvat pian';
+$lang['To change this setting go to:'] = 'Vaihtaaksesi tämän asetuksen, mene:';
+$lang['User notifications'] = 'Käyttäjäilmoitukset';
+$lang['What should piwigo do once photos expire'] = 'Mitä Piwigon pitäisi tehdä, kun valokuvat vanhenevat?';
+$lang['Where should Piwigo move archived photos'] = 'Mihin Piwigon pitäisi siirtää arkistoidut valokuvat?';
+$lang['You have expiring photos'] = 'Sinulla on vanhentuvia kuvia';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Sait tämän sähköpostin, koska latasit aiemmin nämä kuvat: %s';
+$lang['%s, in %s days'] = '%s, %s päivässä';
+$lang['14 days before expiry'] = '14 päivää ennen vanhentumista';
+$lang['30 days before expiry'] = '30 päivää ennen vanhentumista';
+$lang['7 days before expiry'] = '7 päivää ennen vanhentumista';
+$lang['A set number of days before a email will be sent'] = 'Luo ennakkoilmoitus sähköpostiin 7, 14 tai 30 päivää ennen vanhentumispäivää';
+$lang['Action on expiration'] = 'Toimenpide vanhentuessa';
+$lang['Add additional content to email'] = 'Lisää sisältöä sähköpostiin';
+$lang['Administrator notifications'] = 'Järjestelmänvalvojan ilmoitukset';
+$lang['Archive expired photos'] = 'Arkistoi vanhentuneet kuvat';
+$lang['Delete expired photos'] = 'Poista vanhentuneet kuvat';
+$lang['Do nothing'] = 'Älä tee mitään';
+$lang['Expires in'] = 'Vanhentuu ajassa';
+$lang['Expiring photos'] = 'Vanhentuvat kuvat';
+$lang['Expiry date'] = 'Vanhentumispäivä';
+$lang['Expiry date, action has been taken'] = 'Vanhentumispäivä, toimenpiteisiin on ryhdytty';
+$lang['No action was taken on these images'] = 'Ei toimenpidettä tehty näille kuville.';
+$lang['No prenotification'] = 'Ei ennakkoilmoitusta';
+$lang['Notify admins before the expiry date'] = 'Muistuta järjestelmänvalvojia ennen vanhentumispäivää';
+$lang['Notify admins of photo expiration'] = 'Muistuta järjestelmänvalvojia kuvien vanhentumisesta';
+$lang['Notify downloaders of photo expiration'] = 'Muistuta lataajia kuvan vanhentumisesta';
