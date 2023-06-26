434ac65442d76ef2dc580ae55898edae9a6e0ccd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-26 06:56:04 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Therefore these images have been archived in the album %s'] = 'Dlatego te zdjęcia zostały zarchiwizowane w albumie %s';
+$lang['These images have reached expiration: %s'] = 'The zdjęcia wygasły: %s';
+$lang['These photo have reached their expiry date.'] = 'Te zdjęcia osiągnęły datę wygaśnięcia.';
+$lang['These photos will soon expire.'] = 'Te zdjęcia wkrótce wygasną';
+$lang['To change this setting go to:'] = 'Aby zmienić to ustawienie, przejdź do:';
+$lang['User notifications'] = 'Powiadomienia użytkowników';
+$lang['What should piwigo do once photos expire'] = 'Co Piwigo powinno zrobić, kiedy zdjęcia wygasną?';
+$lang['Where should Piwigo move archived photos'] = 'Gdzie Piwigo powinno przenieść archiwizowane zdjęcia?';
+$lang['You have expiring photos'] = 'Masz wygasłe zdjęcia';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Otrzymałaś/eś tę wiadomość, ponieważ poprzednio pobrałaś/eś te zdjęcia: %s';
+$lang['You must select an album for archiving photos'] = 'Musisz wybrać album dla archiwizowanych zdjęć';
+$lang['expired on'] = 'wygasły';
+$lang['expires on'] = 'wygasają';
+$lang['less than %s days'] = 'Mniej niż %s dni';
+$lang['more than than %s days'] = 'Więcej niż %s dni';
+$lang['remove expiry date'] = 'usuń datę wygaśnięcia';
+$lang['the past (already expired)'] = 'Wcześniej (już wygasły)';
+$lang['Notify users before the expiry date'] = 'Powiadom użytkowników przed datą wygaśnięcia';
+$lang['On the expiry date, an email will be sent to all admins'] = 'W dniu wygasania wiadomość będzie wysłana do administratorów';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'W dniu wygasania wiadomość będzie wysłana, aby powiadomić wszystkich, którzy pobrali zdjęcie.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo wie, czy użytkownik pobrał zdjęcie tylko jeżeli historia jego odwiedzin jest zachowana.';
+$lang['%s, in %s days'] = '%s, za %s dni';
+$lang['14 days before expiry'] = '14 dni przed wygaśnięciem';
+$lang['30 days before expiry'] = '30 dni przed wygaśnięciem';
+$lang['7 days before expiry'] = '7 dni przed wygaśnięciem';
+$lang['A set number of days before a email will be sent'] = 'Utwórz przypomnienie aby wysłać wiadomość 7, 14 lub 30 dni przed datą wygaśnięcia';
+$lang['Action on expiration'] = 'Działanie po wygaśnięciu';
+$lang['Add additional content to email'] = 'Dodaj dodatkową zawartość do wiadomości';
+$lang['Administrator notifications'] = 'Powiadomienia administratora';
+$lang['Archive expired photos'] = 'Zarchiwizuj wygasłe zdjęcia';
+$lang['Delete expired photos'] = 'Usuń wygasłe zdjęcia';
+$lang['Do nothing'] = 'Nic nie rób';
+$lang['Expires in'] = 'Wygasa za';
+$lang['Expiring photos'] = 'Wygasające zdjęcia';
+$lang['Expiry date'] = 'Data wygaśnięcia';
+$lang['Expiry date, action has been taken'] = 'Data wygaśnięcia, działanie zostało podjęte';
+$lang['No action was taken on these images'] = 'Żadne działanie nie zostało podjęte dla tych zdjęć.';
+$lang['No prenotification'] = 'Brak przypomnienia';
+$lang['Notify admins before the expiry date'] = 'Powiadom administratorów przed datą wygaśnięcia';
+$lang['Notify admins of photo expiration'] = 'Powiadom administratorów o wygasaniu zdjęć';
+$lang['Notify downloaders of photo expiration'] = 'Powiadom pobierających o wygasaniu zdjęć';
