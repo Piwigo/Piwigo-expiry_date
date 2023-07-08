66d6f969849f08a6311cc38ead3b8e012f344303
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:24:19 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s, in %s days'] = '%s, через %s дней';
+$lang['14 days before expiry'] = '14 дней до истечения срока действия';
+$lang['30 days before expiry'] = '30 дней до истечения срока действия';
+$lang['7 days before expiry'] = '7 дней до истечения срока действия';
+$lang['A set number of days before a email will be sent'] = 'Создайте предварительное уведомление для отправки электронного письма за 7, 14 или 30 дней до истечения срока действия';
+$lang['Action on expiration'] = 'Действие по истечении срока действия';
+$lang['Add additional content to email'] = 'Добавить дополнительный контент в электронную почту';
+$lang['Administrator notifications'] = 'Уведомления администратора';
+$lang['Archive expired photos'] = 'Архивировать фотографии с истекшим сроком действия';
+$lang['Delete expired photos'] = 'Удалить фотографии с истекшим сроком действия';
+$lang['Do nothing'] = 'Ничего не делать';
+$lang['Expires in'] = 'Истекает';
+$lang['Expiring photos'] = 'Фотографии с истекающим сроком действия';
+$lang['Expiry date'] = 'Дата истечения срока действия';
+$lang['Expiry date, action has been taken'] = 'Срок действия истек, действие выполнено';
+$lang['No action was taken on these images'] = 'Над этими изображениями не было предпринято никаких действий.';
+$lang['No prenotification'] = 'Нет предварительного уведомления';
+$lang['Notify admins before the expiry date'] = 'Уведомить администраторов до истечения срока действия';
+$lang['Notify admins of photo expiration'] = 'Уведомить администраторов об истечении срока действия фотографии';
+$lang['Notify downloaders of photo expiration'] = 'Уведомить скачивающих об истечении срока действия фотографий';
+$lang['Notify users before the expiry date'] = 'Уведомить пользователей до истечения срока действия';
+$lang['On the expiry date, an email will be sent to all admins'] = 'По истечении срока действия электронное письмо будет отправлено всем администраторам';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'По истечении срока действия будет отправлено электронное письмо, чтобы уведомить всех, кто скачал фотографию.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo знает, загрузил ли пользователь фотографию, только если его история посещений сохранена.';
+$lang['Therefore these images have been archived in the album %s'] = 'Поэтому эти изображения были заархивированы в альбоме %s';
+$lang['These images have reached expiration: %s'] = 'Срок действия этих изображений истек: %s';
+$lang['These photo have reached their expiry date.'] = 'У этих фотографий истек срок годности.';
+$lang['These photos will soon expire.'] = 'Срок действия этих фотографий скоро истечет';
+$lang['To change this setting go to:'] = 'Чтобы изменить этот параметр, перейдите к:';
+$lang['User notifications'] = 'Уведомления пользователей';
+$lang['What should piwigo do once photos expire'] = 'Что делать Piwigo после истечения срока действия фотографий?';
+$lang['Where should Piwigo move archived photos'] = 'Куда Piwigo следует перемещать архивные фотографии?';
+$lang['You have expiring photos'] = 'У вас есть фотографии с истекающим сроком действия';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Вы получили это письмо, потому что ранее загрузили эти фотографии: %s';
+$lang['You must select an album for archiving photos'] = 'Вы должны выбрать альбом для архивации фотографий';
+$lang['expired on'] = 'истек';
+$lang['expires on'] = 'истекает';
+$lang['less than %s days'] = 'Менее %s дней';
+$lang['more than than %s days'] = 'Более %s дней';
+$lang['remove expiry date'] = 'удалить дату истечения срока действия';
+$lang['the past (already expired)'] = 'прошлое (уже истекло)';
