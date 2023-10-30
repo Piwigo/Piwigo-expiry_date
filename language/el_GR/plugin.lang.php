4aff6849cc66c5e8e58834b23a07d93c54e185f0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-30 07:28:24 +0100

    Update el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['To change this setting go to:'] = 'Για να αλλάξετε αυτήν τη ρύθμιση, μεταβείτε στο';
+$lang['User notifications'] = 'Ειδοποιήσεις χρηστών';
+$lang['These photos will soon expire.'] = 'Αυτές οι φωτογραφίες θα λήξουν σύντομα';
+$lang['These images have reached expiration: %s'] = 'Αυτές οι εικόνες έχουν λήξει: %s';
+$lang['These photo have reached their expiry date.'] = 'Αυτές οι φωτογραφίες έχουν φτάσει στην ημερομηνία λήξης τους';
+$lang['Therefore these images have been archived in the album %s'] = 'Επομένως, αυτές οι εικόνες έχουν αρχειοθετηθεί στο λε\'υκωμα %s';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Το Piwigo γνωρίζει εάν ένας χρήστης έχει κατεβάσει τη φωτογραφία μόνο εάν έχει αποθηκευτεί το ιστορικό επισκέψεών του.';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Κατά την ημερομηνία λήξης, θα σταλεί ένα email σε όλους τους διαχειριστές';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Κατά την ημερομηνία λήξης, θα σταλεί ένα email για να ειδοποιηθεί όποιος έχει κατεβάσει τη φωτογραφία.';
+$lang['Notify downloaders of photo expiration'] = 'Ειδοποιήστε τους χρήστες λήψης για τη λήξη της φωτογραφίας';
+$lang['Notify users before the expiry date'] = 'Ειδοποιήστε τους χρήστες πριν από την ημερομηνία λήξης';
+$lang['Notify admins of photo expiration'] = 'Ειδοποιήστε τους διαχειριστές για τη λήξη της φωτογραφίας';
+$lang['Notify admins before the expiry date'] = 'Ειδοποιήστε τους διαχειριστές πριν από την ημερομηνία λήξης';
+$lang['No prenotification'] = 'Χωρίς προειδοποίηση';
+$lang['No action was taken on these images'] = 'Δεν έγινε καμία ενέργεια σε αυτές τις εικόνες.';
+$lang['Expires in'] = 'Λήγει σε';
+$lang['Expiring photos'] = 'Φωτογραφίες που λήγουν';
+$lang['Expiry date'] = 'Ημερομηνία λήξης';
+$lang['Expiry date, action has been taken'] = 'Ημερομηνία λήξης, έχουν ληφθεί μέτρα';
+$lang['Do nothing'] = 'Μην κάνεις τίποτα';
+$lang['Delete expired photos'] = 'Διαγραφή φωτογραφιών που έχουν λήξει';
+$lang['Archive expired photos'] = 'Αρχειοθέτηση ληγμένων φωτογραφιών';
+$lang['Administrator notifications'] = 'Ειδοποιήσεις διαχειριστή';
+$lang['Add additional content to email'] = 'Προσθέστε επιπλέον περιεχόμενο στο email';
+$lang['Action on expiration'] = 'Ενέργεια για τη λήξη';
+$lang['A set number of days before a email will be sent'] = 'Δημιουργήστε μια προειδοποίηση για να στείλετε ένα email 7, 14 ή 30 ημέρες πριν από την ημερομηνία λήξης';
+$lang['7 days before expiry'] = '7 ημέρες πριν τη λήξη';
+$lang['30 days before expiry'] = '30 ημέρες πριν τη λήξη';
+$lang['14 days before expiry'] = '14 ημέρες πριν τη λήξη';
+$lang['%s, in %s days'] = '%s, σε %s ημέρες';
