277f566b4e258f12af41a92169fa3487e7a4b19d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-31 06:39:15 +0100

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Notify users before the expiry date'] = 'Notificar utilizadores antes da data de expiração';
+$lang['These photos will soon expire.'] = 'Estas fotos irão expirar em breve';
+$lang['User notifications'] = 'Usar notificações';
+$lang['expired on'] = 'Expirado em';
+$lang['expires on'] = 'Expira em';
+$lang['less than %s days'] = 'Menos de %s dias';
+$lang['more than than %s days'] = 'mais que %s dias';
+$lang['remove expiry date'] = 'remover data de expiração';
+$lang['%s, in %s days'] = '%s, em %s dias ';
+$lang['14 days before expiry'] = '14 dias antes de expirar';
+$lang['30 days before expiry'] = '30 dias antes de expirar';
+$lang['7 days before expiry'] = ' 7 dias antes de expirar';
+$lang['A set number of days before a email will be sent'] = 'Criar uma pré-notificação para enviar um email a 7, 14 ou 30 dias antes da data de expiração.';
+$lang['Action on expiration'] = 'Ação na expiração';
+$lang['Add additional content to email'] = 'Adicionar conteúdo adicional ao email.';
+$lang['Administrator notifications'] = 'Notificações administrador';
+$lang['Archive expired photos'] = 'Arquivo de fotos expiradas';
+$lang['Delete expired photos'] = 'Apagar fotos expiradas';
+$lang['Do nothing'] = 'Nada a fazer';
+$lang['Expires in'] = 'Expira em';
+$lang['Expiring photos'] = 'Fotos expiradas';
+$lang['Expiry date'] = 'Data de expirar';
+$lang['Expiry date, action has been taken'] = 'Data de expirar, ação foi tomada';
+$lang['No action was taken on these images'] = 'Não foi tomada qualquer ação nestas fotos';
+$lang['No prenotification'] = 'Sem pré-notificação';
+$lang['Notify admins before the expiry date'] = 'Notificar administradores antes da data de expirar';
+$lang['Notify admins of photo expiration'] = 'Notificar administradores da expiração da foto';
+$lang['Notify downloaders of photo expiration'] = 'Notificar descarregadores da expiração da foto';
