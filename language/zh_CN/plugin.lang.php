9b9dd30b9feb2258a3979eb732912216d09e8859
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-10-10 07:35:26 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Expires in'] = '有效期：';
+$lang['Expiring photos'] = '过期的照片';
+$lang['Expiry date, action has been taken'] = '到期日期，已采取行动';
+$lang['No action was taken on these images'] = '没有对这些图像采取任何措施。';
+$lang['Therefore these images have been archived in the album %s'] = '因此这些图片被归档到相册 %s 中';
+$lang['These images have reached expiration: %s'] = '这些图像已过期：%s';
+$lang['These photo have reached their expiry date.'] = '这些照片已过期。';
+$lang['These photos will soon expire.'] = '这些照片很快就会过期';
+$lang['You have expiring photos'] = '您有即将过期的照片';
+$lang['You have received this email because you previously downloaded these photos: %s'] = '您收到这封电子邮件是因为您之前下载过这些照片：%s';
+$lang['expired on'] = '已过期';
+$lang['expires on'] = '到期日期';
+$lang['less than %s days'] = '少于%s天';
+$lang['more than than %s days'] = '超过%s天';
+$lang['remove expiry date'] = '删除到期日期';
+$lang['the past (already expired)'] = '过去（已过期）';
+$lang['Expiry date'] = '到期日期';
+$lang['You must select an album for archiving photos'] = '您必须选择一个相册来存档照片';
+$lang['%s, in %s days'] = '%s, 在 %s天内 ';
+$lang['On the expiry date, an email will be sent to all admins'] = '到期时，将向所有管理员发送电子邮件';
+$lang['Add additional content to email'] = '向电子邮件添加其他内容';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo只有保存了用户的访问历史记录才知道用户是否下载了照片。';
+$lang['To change this setting go to:'] = '要更改此设置，请转至：';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = '在到期日，将发送电子邮件通知所有下载了该照片的人。';
+$lang['A set number of days before a email will be sent'] = '创建预先通知，在到期日期前7天、14天或30天发送电子邮件';
+$lang['Notify admins before the expiry date'] = '在到期日期之前通知管理员';
+$lang['Notify users before the expiry date'] = '在到期日期之前通知用户';
+$lang['14 days before expiry'] = '到期前14天';
+$lang['30 days before expiry'] = '到期前30天';
+$lang['7 days before expiry'] = '到期前7天';
+$lang['No prenotification'] = '不提醒';
+$lang['Where should Piwigo move archived photos'] = 'Piwigo 应该将存档的照片移动到哪里？';
+$lang['User notifications'] = '用户通知';
+$lang['Administrator notifications'] = '管理员通知';
+$lang['Notify admins of photo expiration'] = '通知管理员照片到期时间';
+$lang['Notify downloaders of photo expiration'] = '通知下载者照片到期时间';
+$lang['Action on expiration'] = '到期时的操作';
+$lang['Archive expired photos'] = '存档过期的照片';
+$lang['Delete expired photos'] = '删除过期的照片';
+$lang['Do nothing'] = '无操作';
+$lang['What should piwigo do once photos expire'] = '照片过期后，Piwigo应该怎么办？';
