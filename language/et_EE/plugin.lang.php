494130e1480712f9f61b46e58068a356ba160951
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-04 18:22:20 +0100

    Update et_EE, thanks to Exabot

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['No action was taken on these images'] = 'Nende piltide suhtes ei võetud meetmeid.';
+$lang['No prenotification'] = 'Ei mingit eelteadet';
+$lang['Notify admins before the expiry date'] = 'Teavitage administraatoreid enne aegumiskuupäeva';
+$lang['Notify admins of photo expiration'] = 'Teatage administraatoritele foto aegumisest';
+$lang['Notify downloaders of photo expiration'] = 'Teatage allalaadijatele foto aegumisest';
+$lang['Notify users before the expiry date'] = 'Teavitage kasutajaid enne aegumiskuupäeva';
+$lang['On the expiry date, an email will be sent to all admins'] = 'Aegumiskuupäeval saadetakse kõigile administraatoritele e-kiri';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'Aegumiskuupäeval saadetakse e-kiri, et teavitada kõiki, kes on foto alla laadinud.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'Piwigo teab, kas kasutaja on foto alla laadinud ainult siis, kui tema külastusajalugu on salvestatud.';
+$lang['Therefore these images have been archived in the album %s'] = 'Seetõttu on need pildid arhiveeritud albumisse %s';
+$lang['These images have reached expiration: %s'] = 'Need pildid on aegunud: %s';
+$lang['These photo have reached their expiry date.'] = 'Nende fotode kehtivusaeg on lõppenud.';
+$lang['These photos will soon expire.'] = 'Need fotod aeguvad varsti';
+$lang['To change this setting go to:'] = 'Selle sätte muutmiseks minge aadressile:';
+$lang['User notifications'] = 'Kasutajateatised';
+$lang['What should piwigo do once photos expire'] = 'Mida peaks Piwigo tegema, kui fotod aeguvad?';
+$lang['Where should Piwigo move archived photos'] = 'Kuhu peaks Piwigo arhiveeritud fotod teisaldama?';
+$lang['You have expiring photos'] = 'Teil on aeguvad fotod';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'Saite selle e-kirja, kuna olete varasemalt alla laadinud järgmised fotod: %s';
+$lang['You must select an album for archiving photos'] = 'Fotode arhiveerimiseks peate valima albumi';
+$lang['expired on'] = 'aegus';
+$lang['expires on'] = 'aegub';
+$lang['less than %s days'] = 'Vähem kui %s päeva';
+$lang['more than than %s days'] = 'Rohkem kui %s päeva';
+$lang['remove expiry date'] = 'eemaldage aegumiskuupäev';
+$lang['the past (already expired)'] = 'minevik (juba aegunud)';
+$lang['Action on expiration'] = 'Toiming aegumise korral';
+$lang['Expires in'] = 'Aegub aastal';
+$lang['Expiring photos'] = 'Aeguvad fotod';
+$lang['Expiry date'] = 'Aegumiskuupäev';
+$lang['Expiry date, action has been taken'] = 'Aegumiskuupäev, meetmed on võetud';
+$lang['A set number of days before a email will be sent'] = 'Looge eelteatis, et saata e-kiri 7, 14 või 30 päeva enne aegumiskuupäeva';
+$lang['7 days before expiry'] = '7 päeva enne aegumist';
+$lang['30 days before expiry'] = '30 päeva enne aegumist';
+$lang['14 days before expiry'] = '14 päeva enne aegumist';
+$lang['%s, in %s days'] = '%s, %s päeva pärast';
+$lang['Add additional content to email'] = 'Lisage meilile täiendavat sisu';
+$lang['Administrator notifications'] = 'Administraatori märguanded';
+$lang['Archive expired photos'] = 'Aegunud fotode arhiiv';
+$lang['Delete expired photos'] = 'Kustutage aegunud fotod';
+$lang['Do nothing'] = 'Ära tee midagi';
