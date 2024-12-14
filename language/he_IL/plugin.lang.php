dbe12e1103253543b7b617357f72de5a545b110d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-14 06:38:53 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the past (already expired)'] = 'העבר (כבר פג תוקפו)';
+$lang['Notify users before the expiry date'] = 'הודע למשתמשים לפני תאריך התפוגה';
+$lang['On the expiry date, an email will be sent to all admins'] = 'בתאריך התפוגה, יישלח אימייל לכל המנהלים';
+$lang['On the expiry date, an email will be sent to notify anyone who has downloaded the photo.'] = 'בתאריך התפוגה, יישלח אימייל להודיע לכל מי שהוריד את התמונה.';
+$lang['Piwigo knows if a user has downloaded the photo only if their visit history is saved.'] = 'פיוויגו יודע אם משתמש הוריד את התמונה רק אם היסטוריית הביקורים שלו נשמרה.';
+$lang['Therefore these images have been archived in the album %s'] = 'לכן תמונות אלו אוחסנו בארכיון באלבום %s';
+$lang['These images have reached expiration: %s'] = 'תמונות אלו הגיעו לתום תוקפן: %s';
+$lang['These photo have reached their expiry date.'] = 'תמונות אלו הגיעו לתאריך תפוגתן.';
+$lang['These photos will soon expire.'] = 'תמונות אלו יפוגו בקרוב';
+$lang['To change this setting go to:'] = 'כדי לשנות הגדרה זו, עבור ל-:';
+$lang['User notifications'] = 'הודעות משתמש';
+$lang['What should piwigo do once photos expire'] = 'מה על פיוויגו לעשות ברגע שהתמונות פגו תוקפן?';
+$lang['Where should Piwigo move archived photos'] = 'איפה על פיוויגו להעביר תמונות שארכיון?';
+$lang['You have expiring photos'] = 'יש לך תמונות שפגות תוקפן';
+$lang['You have received this email because you previously downloaded these photos: %s'] = 'קיבלת אימייל זה מכיוון שהורדת בעבר את התמונות הללו: %s';
+$lang['You must select an album for archiving photos'] = 'עליך לבחור אלבום לארכוב תמונות';
+$lang['expired on'] = 'פגו תוקף ב-';
+$lang['expires on'] = 'פוקעות ב-';
+$lang['less than %s days'] = 'פחות מ-%s ימים';
+$lang['more than than %s days'] = 'יותר מ-%s ימים';
+$lang['remove expiry date'] = 'הסר את תאריך התפוגה';
+$lang['%s, in %s days'] = '%s, בעוד %s ימים';
+$lang['14 days before expiry'] = '14 ימים לפני תום התוקף';
+$lang['30 days before expiry'] = '30 ימים לפני תום התוקף';
+$lang['7 days before expiry'] = '7 ימים לפני תום התוקף';
+$lang['A set number of days before a email will be sent'] = 'צור התראה מוקדמת לשליחת אימייל 7, 14 או 30 ימים לפני תאריך התפוגה';
+$lang['Action on expiration'] = 'פעולה עם תום התוקף';
+$lang['Add additional content to email'] = 'הוסף תוכן נוסף לאימייל';
+$lang['Administrator notifications'] = 'הודעות מנהל';
+$lang['Archive expired photos'] = 'העבר לארכיון תמונות שפגו תוקפן';
+$lang['Delete expired photos'] = 'מחק תמונות שפגו תוקפן';
+$lang['Do nothing'] = 'אל תעשה כלום';
+$lang['Expires in'] = 'פג תוקף בעוד';
+$lang['Expiring photos'] = 'תמונות שפגות תוקפן';
+$lang['Expiry date'] = 'תאריך תפוגה';
+$lang['Expiry date, action has been taken'] = 'תאריך תפוגה, ננקטה פעולה';
+$lang['No action was taken on these images'] = 'לא ננקטה פעולה על תמונות אלו.';
+$lang['No prenotification'] = 'ללא התראה מוקדמת';
+$lang['Notify admins before the expiry date'] = 'הודע למנהלים לפני תאריך התפוגה';
+$lang['Notify admins of photo expiration'] = 'הודע למנהלים על תפוגת התמונות';
+$lang['Notify downloaders of photo expiration'] = 'הודע למורידי התמונות על תפוגת התמונות';
